58c19b82cd563025d7f99e0050a0786c3e3d9e44
Aboneh Manyazewal <aboneh@gmail.com>
2021-10-01 19:59:07 -0500

    Update functions.js

--- basic_javascript/functions.js
+++ basic_javascript/functions.js
@@ -1,20 +1,3 @@
-// test function
-function myFunctionTest(expected, found){
-    if(expected===found){
-        return "TEST SUCCEEDED";
-    }else{
-        return "TEST FAILED. Expected output is " + expected + " but found " + found;
-    }
-};
-
-/* test func for arrays */
-function myTestFunctionArrays(expected, found){
-    if(JSON.stringify(expected) == JSON.stringify(found)){
-        return "TEST SUCCEEDED";
-    }else{
-        return "TEST FAILED. Expected [" + expected + "]" + "found" + found;
-    }
-};
 
 // maximum of two
 function max(a,b){
@@ -27,7 +10,7 @@ function max(a,b){
 console.log("Expected output of max(20,50) is 50 " + myFunctionTest(50,max(20,50)));
 console.log("Expected output of max(20,50) is 50 " + myFunctionTest(20,max(20,50)));
 
-//return the max of three numbers 
+//maximum of three numbers 
 function maxOfThree(a, b, c){
     var max = a;
     if(max < b){
@@ -39,31 +22,30 @@ function maxOfThree(a, b, c){
     return max;
 };
 
-console.log("Expected output of maxOfThree(5,4,44) is 44  " + myFunctionTest(44, maxOfThree(5, 4, 44)));
-console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest(55, maxOfThree(55, 4, 44)));
+console.log("Expected output of maxOfThree(3,5,2) is 5  " + myFunctionTest(5, maxOfThree(3,5,2)));
 
-//return true if character is a vowel 
+//is a vowel 
 function isVowel(c){
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     return vowels.includes(c);
 };
+console.log("Expected output of isVowel('p') is false " + myFunctionTest(false, isVowel('p')));
+console.log("Expected output of isVowel('o') is true " + myFunctionTest(true, isVowel('o')));
 
-console.log("Expected output of isVowel('a') is true " + myFunctionTest(true, isVowel('a')));
-console.log("Expected output of isVowel('f') is false " + myFunctionTest(false, isVowel('f')));
 
-//sum elements in the array 
-function sum(numbers){
-    let sum = numbers.reduce(function(accumulator, currentValue){
-        return accumulator + currentValue;
+//sum of elements in the array 
+function sum(num){
+    let sum = num.reduce(function(accumulator, value){
+        return accumulator + value;
     });
     return sum;
 };
 
 console.log("Expected output of sum([1,2,3,4,5]) is 15 " + myFunctionTest(15, sum([1,2,3,4,5])));
 
-//multuply each elements in the array 
-function multuply(numbers){
-    let result = numbers.reduce((a, b) => a * b);
+//multuply each elements of the array
+function multuply(num){
+    let result = num.reduce((a, b) => a * b);
     return result;
 };
 
@@ -76,7 +58,7 @@ function reverse(str){
 
 console.log("Expected output of reverse('jag testar') is 'ratset gaj' " + myFunctionTest("ratset gaj", reverse("jag testar")));
 
-//findLongestWord 
+//find Longest Word 
 function findLongestWord(words){
     var length = words[0].length;
     for(i = 1; i < words.length; i++){
@@ -87,9 +69,10 @@ function findLongestWord(words){
     return length;
 };
 
-console.log("Expected output of findLongestWord(['jos', 'jo', 'yoseph']) is 6 " + myFunctionTest(6, findLongestWord(['jos', 'jo', 'yoseph'])));
+console.log("Expected output of findLongestWord(['Manyazewal', 'Ab', 'Aboneh']) is 6 " + myFunctionTest(6, findLongestWord(['Manyazewal', 'Ab', 'Aboneh'])));
+console.log("Expected output of findLongestWord(['Manyazewal', 'Ab', 'Aboneh']) is 6 " + myFunctionTest(10, findLongestWord(['Manyazewal', 'Ab', 'Aboneh'])));
 
-//function findLongestWords */
+//function find Longest Words 
 function findLongestWords(k,words){
     var newWords = [];
     for(i = 0; i < words.length; i++){
@@ -102,45 +85,41 @@ function findLongestWords(k,words){
 
 console.log("Expected output of findLongestWords(['Aboneh', 'Ab', 'Manyazewal']) is ['Aboneh', 'Manyazewal'] " + myTestFunctionArrays(["Aboneh", "Manyazewal"], myTestFunctionArrays(3, ['Aboneh', 'Ab', 'Manyazewal'])));
 
+//Modify the jsfiddle on the map/filter/reduce slide
 const a = [1,3,5,3,3]; 
 
-const b = a.map(function(elem, i, array) {
-return elem + 3;
-})
-
-
-document.writeln(b.toString() + "<br/>");
-
-const c = a.filter(function(elem, i, array){
-return elem !== 3;});
-
-document.writeln(c.toString() + "<br/>");
-
-const d = a.reduce(function(prevValue, elem, i, array){
-return prevValue + elem;
-});
-document.writeln(d+ "<br/>");
-
-const d2 = a.find(function(elem) {return elem > 1;}); //3
-const d3 = a.findIndex(function(elem) {return elem > 1;}); //1
-
-
-document.writeln(d2+ "<br/>");
-document.writeln(d3);
-
-
-const multiplyBy10 = a.map(function(elem, i, array){
+const multiplyByTen = a.map(function(elem, i, array){
 return elem * 10;
 });
-document.writeln("<br/>");
-document.writeln(multiplyBy10.toString() + "<br/>");
 
-const only3 = a.filter(function(x){
+document.writeln("Each element multiplied by 10: " + multiplyByTen.toString());
+
+const onlyThree = a.filter(function(x){
 return x == 3;
 });
 document.writeln("<br/>");
-document.writeln(only3.toString() + "<br/>");
+document.writeln("Array with all elements equal to 3: " + onlyThree.toString());
 
 const product = a.reduce((a,b) => a * b);
 document.writeln("<br/>");
-document.writeln(product.toString() + "<br/>");
+document.writeln("The product of all elements: " + product.toString());
+
+
+//Test functions
+// test function
+function myFunctionTest(expected, found){
+    if(expected===found){
+        return "TEST SUCCEEDED";
+    }else{
+        return "TEST FAILED. Expected output is " + expected + " but found " + found;
+    }
+};
+
+//test function for arrays
+function myTestFunctionArrays(expected, found){
+    if(JSON.stringify(expected) == JSON.stringify(found)){
+        return "TEST SUCCEEDED";
+    }else{
+        return "TEST FAILED";
+    }
+};

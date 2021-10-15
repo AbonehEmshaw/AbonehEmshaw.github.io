b21e5d93a0dec44bab1925d75ef91fdc7d616b83
Aboneh Manyazewal <aboneh@gmail.com>
2021-10-15 14:03:18 -0500

    commit all

--- w3d5/q3/index.js
+++ w3d5/q3/index.js
@@ -1,32 +1,26 @@
 const express = require('express');
+const path=require("path");
 const app = express();
-app.use("/result",express.urlencoded({extended:false}));
+
 app.get('/', (req, res) => {
-    
+    const date=new Date();
+    const hour=date.getHours();
+
     let response="<!DOCTYPE html>"+
     "<html>"+
         "<head>"+
-            "<title>My Express App</title>"+
+            "<title>My App</title>"+
+            `<link href="${hour >= 6 && hour <=18 ?'/css/day.css':'/css/night.css'}" rel="stylesheet">`+
         "</head>"+
         "<body>"+
             "<form action='/result' method='POST'>"+
                 "<label>Name <input type='text' name='name'></label>"+
                 "<label>Age <input type='text' name='age'></label>"+
-                "<input type='submit' value='submit Query'></label>"+
+                "<input type='submit' value='submit query'></label>"+
             "</form>"+
         "</body>"+
     "</html>";
     res.send(response);
 });
-app.post('/result', (req, res) => {
-    let name = req.body.name;
-    let age = req.body.age;
-    if (!name) {
-        name = "person";
-    }
-    if(!age){
-        age="unknown";
-    }
-    res.send(`Welcome ${name} with age ${age}`);
-});
+app.use('/css', express.static(path.join(__dirname, 'css')));
 app.listen(3000);

db7db2582d273999632d2e6d7e0201507ebf455a
Aboneh Manyazewal <aboneh@gmail.com>
2021-10-16 14:26:50 -0500

    commit all

--- w3d6/q1/index.js
+++ w3d6/q1/index.js
@@ -1,8 +1,10 @@
 const express = require('express'); 
 const path = require('path');
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "view"));
+
 app.get('/', (req, res) => {
     const date = new Date();
     res.render("index", {
@@ -10,5 +12,6 @@ app.get('/', (req, res) => {
     hour:date.getHours()
  });
 });
+
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.listen(3000);

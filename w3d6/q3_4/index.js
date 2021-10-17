359e7622b974dc3a232b68a9065254065c004214
Aboneh Manyazewal <aboneh@gmail.com>
2021-10-17 14:33:25 -0500

    commit all

--- w3d6/q3_4/index.js
+++ w3d6/q3_4/index.js
@@ -6,15 +6,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 const products = [
     { id: 1, name: 'product 1', description: "product description", price: 10},
-    { id: 2, name: 'product 1', description: "product description", price: 20},
-    { id: 3, name: 'product 1', description: "product description", price: 30},
-    { id: 4, name: 'product 1', description: "product description", price: 40},
-    { id: 5, name: 'product 1', description: "product description", price: 50},
-    { id: 6, name: 'product 1', description: "product description", price: 60},
-    { id: 7, name: 'product 1', description: "product description", price: 70},
-    { id: 8, name: 'product 1', description: "product description", price: 80},
-    { id: 9, name: 'product 1', description: "product description", price: 90},
-    { id: 10, name: 'product 1', description: "product description", price: 100},
+    { id: 2, name: 'product 2', description: "product description", price: 20},
+    { id: 3, name: 'product 3', description: "product description", price: 30},
+    { id: 4, name: 'product 4', description: "product description", price: 40},
+    { id: 5, name: 'product 5', description: "product description", price: 50},
+    { id: 6, name: 'product 6', description: "product description", price: 60},
+    { id: 7, name: 'product 7', description: "product description", price: 70},
+    { id: 8, name: 'product 8', description: "product description", price: 80},
+    { id: 9, name: 'product 9', description: "product description", price: 90},
+    { id: 10, name:'product 10', description: "product description", price: 100},
     ];
 
 const shopCart={};
